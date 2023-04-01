ec9e20fe7d019bd68e245b3c1bad8eea44feb59f
Darkgoatie <81323822+Darkgoatie@users.noreply.github.com>
2023-04-01 19:31:42 +0300

    Update

--- src/Routes/Home/Members/Members.js
+++ src/Routes/Home/Members/Members.js
@@ -2,29 +2,48 @@ import "./Members.css";
 
 const Members = () => {
   let members = require("./TeamMembers.json");
+  let personalLinks = require("./MemberLinks.json");
+
+  const compareStrings = (a, b) => {
+    if (a < b) return -1;
+    if (a > b) return 1;
+
+    return 0;
+  };
+
+  const compare = (a, b) => {
+    const splitA = a.split(" ");
+    const splitB = b.split(" ");
+    const lastA = splitA[splitA.length - 1];
+    const lastB = splitB[splitB.length - 1];
+
+    return lastA === lastB
+      ? compareStrings(splitA[0], splitB[0])
+      : compareStrings(lastA, lastB);
+  };
+
+  members = members.sort(compare);
+
   let m = Math.ceil(members.length / 2);
   let firstHalf = members.slice(0, m);
   let secondHalf = members.slice(m);
+  let memberLink;
 
   const arrayToItems = (array) => {
     return array.map((i) => {
-      return <li>{i}</li>;
+      memberLink = personalLinks[i];
+      return (
+        <li>
+          <a className="memberLink" target="_blank" href={memberLink}>
+            {i}
+          </a>
+        </li>
+      );
     });
   };
-
-  const svgStar = (
-    <img
-      title="Team Captain"
-      style={{
-        height: "20px",
-      }}
-      src="svgs/star.svg"
-    ></img>
-  );
-
   return (
     <div className="section dark" id="Members">
-      <div className="jumbotron">
+      <div className="NAMESJUMBOTRON jumbotron">
         <p className="display-4">Çalışma Grubu</p>
         <p
           style={{
@@ -33,14 +52,10 @@ const Members = () => {
         ></p>
         <div className="row">
           <div className="col-sm-6">
-            <ul>
-              {arrayToItems(firstHalf)}
-            </ul>
+            <ul>{arrayToItems(firstHalf)}</ul>
           </div>
           <div className="col-sm-6">
-            <ul>
-              {arrayToItems(secondHalf)}
-            </ul>
+            <ul>{arrayToItems(secondHalf)}</ul>
           </div>
         </div>
       </div>
